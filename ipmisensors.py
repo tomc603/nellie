213f54901cb03dcae30af5c6cda29df038950daa
Tom Cameron <tomc603@gmail.com>
2021-04-28 23:58:32 -0400

    Complete conversion to use a class

--- ipmisensors.py
+++ ipmisensors.py
@@ -9,11 +9,35 @@ class SensorData(object):
     timestamp = None
     readings = {}
 
+    def __init__(self, data=None):
+        if data is not None:
+            self._parse(data)
+
     def __gt__(self, other):
         return self.timestamp > other.timestamp
 
     def __lt__(self, other):
         return self.timestamp < other.timestamp
+
+    def _parse(self, data):
+        """
+
+        :param data: Text output from ipmi sensors command
+        :type data: str
+        """
+        self.timestamp = time.time()
+
+        for line in data.splitlines():
+            columns = line.split('|')
+            label, value = columns[0].strip(), columns[1].strip()
+
+            # Clean up some known data types
+            if "." in value:
+                value = float(value)
+            elif value == "na":
+                value = 0
+
+            self.readings[label] = value
 
 
 def call_stats():
@@ -31,64 +55,44 @@ def call_stats():
     return process
 
 
-def parse_ipmi_output(stats_text):
-    """
-
-    :param stats_text:
-    :type stats_text: str
-    :return: Dictionary of sensor names an values
-    :rtype dict
-    """
-    data = {}
-
-    for line in stats_text.splitlines():
-        columns = line.split('|')
-        label, value = columns[0].strip(), columns[1].strip()
-        if "." in value:
-            value = float(value)
-        elif value == "na":
-            value = 0
-        data[label] = value
-    return data
-
-
 def collect_stats(interval, iterations):
     """
 
-    :return: Dictionary with timestamps as the keys, and parse_ipmi_output as values
-    :rtype: dict[int, dict[Any, Union[int, float]]]
+    :return: List of SensorData objects containing readings for each iteration
+    :rtype: list[SensorData]
     """
     iteration = 0
-    stats = {}
+    stats = []
 
     while iteration < iterations:
         iteration += 1
-        timestamp = int(time.time())
         start_time = time.time()
         p = call_stats()
-        d = parse_ipmi_output(p.stdout)
-        stats[timestamp] = d
+        # d = parse_ipmi_output(p.stdout)
+        # stats[timestamp] = d
+        d = SensorData(p.stdout)
+        stats.append(d)
         runtime = time.time() - start_time
         time.sleep(max(0.0, interval - runtime))
     return stats
 
 
-def stats_to_rows(stats):
+def stats_to_rows(data):
     """
 
+    :type data: list[SensorData]
     :rtype: list, list
     """
     column_labels = ['timestamp', ]
     output_rows = []
-    timestamps = sorted(stats.keys())
 
-    for timestamp in timestamps:
-        output_row = [timestamp, ]
-        for k in stats[timestamp]:
+    for sensor_data in data:
+        output_row = [sensor_data.timestamp, ]
+        for k in sensor_data.readings:
             if k not in column_labels:
                 column_labels.append(k)
         for label in column_labels[1:]:
-            output_row.append(stats[timestamp][label])
+            output_row.append(sensor_data.readings[label])
         output_rows.append(output_row)
     return column_labels, output_rows
 
@@ -97,7 +101,8 @@ def print_stats(stats):
     header, rows = stats_to_rows(stats)
 
     print(','.join(header))
-    print(','.join(rows))
+    for row in rows:
+        print(row)
 
 
 def stats_to_csv(stats):
